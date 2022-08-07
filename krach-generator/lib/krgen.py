c21b0f3937646f1e397688d48e24af68f8dd23f8
Blickfeldkurier <Blickfeldkurier@posteo.de>
2022-08-07 09:51:49 +0200

    feat: move soundfile data to class level and make app quit when q is pressed

--- krach-generator/lib/krgen.py
+++ krach-generator/lib/krgen.py
@@ -9,50 +9,54 @@ from playsound import playsound
 
 
 class Krach:
-    def setupGPIO(self, mode, pin):
+    def setupGPIO(self, mode, pin: int) -> None:
         if self.haz_gpio:
             GPIO.setwarnings(False) # Ignore warning for now
             GPIO.setmode(mode) # Use physical pin numbering: GPIO.BOARD
             GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
-    def __init__(self) -> None:
+    def getSoundFiles(self) -> list:
+        if not os.path.isdir(self.sounddir):
+            return []
+        return os.listdir(self.sounddir)
+
+    def __init__(self, sounddir:str) -> None:
+        self.sounddir = sounddir
+        self.soundfiles = self.getSoundFiles()
+        if self.soundfiles == []:
+            print("No sounds to play - exiting...")
+            exit(0)
+
         gpio_spec = importlib.util.find_spec("RPi")
         self.haz_gpio = gpio_spec is not None
         if self.haz_gpio:
             import RPi.GPIO as GPIO
             self.setupGPIO(GPIO.BOARD, 10)
-
-    def getSoundFiles(self, sounddir: str) -> list:
-        if not os.path.isdir(sounddir):
-            return []
-        return os.listdir(sounddir)
     
-    def playsound(self, sounddir, snd_file):
+    def playsound(self, snd_file:str) -> None:
         print("soundfile: " + str(snd_file))
-        playsound(sounddir + os.sep + snd_file)
+        playsound(self.sounddir + os.sep + snd_file)
         
-    def mainloop(self, sounddir : str):
-        file_paths = self.getSoundFiles(sounddir)
-        if file_paths == []:
-            print("No sounds to play - exiting...")
-            exit(0)
+    def mainloop(self):
         
         while True:
-            snd_file = random.choice(file_paths)
+            snd_file = random.choice(self.soundfiles)
             if self.haz_gpio:
                 if GPIO.input(10) == GPIO.HIGH:
-                    self.playsound(sounddir, snd_file)
+                    self.playsound(snd_file)
             else:
-                keypressed = input("Press any key")
-                self.playsound(sounddir, snd_file)
+                inputstr = input("Press any key, q to quit:\n")
+                if inputstr.startswith("q"):
+                    exit(0)
+                self.playsound(snd_file)
             
 def main():
     parser = argparse.ArgumentParser(description='Bring the noise')
     parser.add_argument('-s','--sounds', default=os.getcwd()+os.sep+"sounds", help="Sound directory (Default: cwd/sounds")
     args = parser.parse_args()
 
-    gerausch = Krach()
-    gerausch.mainloop(os.path.abspath(args.sounds))
+    gerausch = Krach(os.path.abspath(args.sounds))
+    gerausch.mainloop()
 
 if __name__ == '__main__':
     main()
